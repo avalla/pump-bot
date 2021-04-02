c84b93fe298eae74c9887a311eb0512ae5edb6aa
Andrea Valla <valla.andrea@gmail.com>
2021-04-02 13:01:22 +0200

    fix tests

--- helpers/round.js
+++ helpers/round.js
@@ -4,7 +4,7 @@
  * @returns {number}
  */
 function round(number) {
-  return Math.ceil((number + Number.EPSILON) * 1_000_000) / 1_000_000;
+  return Math.round((number) * 1_000_000) / 1_000_000;
 }
 
 module.exports = round;
--- main.test.js
+++ main.test.js
@@ -2,12 +2,9 @@ const debug = require('debug')('pump-bot:tests');
 const Trader = require('node-binance-api');
 const trading = require('./services/trading');
 const binance = new Trader().options();
-const configuration = {
-  thresholds: {
-    USDT: 10,
-    BTC: 0.0001,
-  },
-};
+
+BTC_THRESHOLD = 0.0001;
+USDT_THRESHOLD = 10;
 
 describe('Pump bot tests', () => {
   describe('Stop price', () => {
@@ -16,7 +13,7 @@ describe('Pump bot tests', () => {
       const stopLoss = 0.2; // 20%
       const quantity = 100;
       const main = 'USDT';
-      const options = { marketPrice, stopLoss, quantity, main, configuration };
+      const options = { marketPrice, stopLoss, quantity, main, threshold: USDT_THRESHOLD };
       const stopPrice = await trading.stopPrice(binance, options);
       expect(stopPrice).toBe(8);
     });
@@ -25,7 +22,7 @@ describe('Pump bot tests', () => {
       const stopLoss = 0.2; // 20%
       const quantity = 10000;
       const main = 'USDT';
-      const options = { marketPrice, stopLoss, quantity, main, configuration };
+      const options = { marketPrice, stopLoss, quantity, main, threshold: USDT_THRESHOLD };
       const stopPrice = await trading.stopPrice(binance, options);
       expect(stopPrice).toBe(0.08);
     });
@@ -34,7 +31,7 @@ describe('Pump bot tests', () => {
       const stopLoss = 0.1; // 10%
       const quantity = 10;
       const main = 'USDT';
-      const options = { marketPrice, stopLoss, quantity, main, configuration };
+      const options = { marketPrice, stopLoss, quantity, main, threshold: USDT_THRESHOLD };
       const stopPrice = await trading.stopPrice(binance, options);
       expect(stopPrice).toBe(1);
     });
@@ -43,7 +40,7 @@ describe('Pump bot tests', () => {
       const stopLoss = 0.5; // 5%
       const quantity = 100000;
       const main = 'USDT';
-      const options = { marketPrice, stopLoss, quantity, main, configuration };
+      const options = { marketPrice, stopLoss, quantity, main, threshold: USDT_THRESHOLD };
       const stopPrice = await trading.stopPrice(binance, options);
       expect(stopPrice).toBe(0.0005);
     });
@@ -53,57 +50,47 @@ describe('Pump bot tests', () => {
     const pair = 'WINUSDT';
     it('Sell price #1 - LOW 5 | PROFIT 100% => 10', async () => {
       const profit = 1; // 100%
-      binance.setLow(5);
-      const options = { pair, profit, interval };
+      const lowPrice = 5;
+      const options = { profit, lowPrice };
       const sellPrice = await trading.sellPrice(binance, options);
       expect(sellPrice).toBe(10);
     });
     it('Sell price #2 - LOW 5 | PROFIT 200% => 15', async () => {
       const profit = 2; // 200%
-      binance.setLow(5);
-      const options = { pair, profit, interval };
+      const lowPrice = 5;
+      const options = { profit, lowPrice };
       const sellPrice = await trading.sellPrice(binance, options);
       expect(sellPrice).toBe(15);
     });
     it('Sell price #3 - LOW 5 | PROFIT 50% => 7.5', async () => {
       const profit = 0.5; // 50%
-      binance.setLow(5);
-      const options = { pair, profit, interval };
+      const lowPrice = 5;
+      const options = { profit, lowPrice };
       const sellPrice = await trading.sellPrice(binance, options);
       expect(sellPrice).toBe(7.5);
       binance.setLow(undefined);
     });
     it('Sell price #4 - LOW 10 | PROFIT 100% => 20', async () => {
-      binance.setLow(10);
       const profit = 1; // 100%
-      const options = { pair, profit, interval };
+      const lowPrice = 10;
+      const options = { profit, lowPrice };
       const sellPrice = await trading.sellPrice(binance, options);
       expect(sellPrice).toBe(20);
     });
     it('Sell price #4 - LOW 0.5 | PROFIT 100% => 1', async () => {
-      binance.setLow(0.5);
       const profit = 1; // 100%
-      const options = { pair, profit, interval };
+      const lowPrice = 0.5;
+      const options = { profit, lowPrice };
       const sellPrice = await trading.sellPrice(binance, options);
       expect(sellPrice).toBe(1);
     });
     it('Sell price #5 - LOW 0.05 | PROFIT 25% => 0.625', async () => {
-      binance.setLow(0.5);
       const profit = 0.25; // 25%
-      const options = { pair, profit, interval };
+      const lowPrice = 0.5;
+      const options = { profit, lowPrice };
       const sellPrice = await trading.sellPrice(binance, options);
       expect(sellPrice).toBe(0.625);
     });
 
-    // it('Sell price #3', async () => {
-    //   binance.setLow(10)
-    //   const interval = '15m';
-    //   const profit = .5;
-    //   const pair = 'WINUSDT';
-    //   const options = { pair, profit, interval };
-    //   const sellPrice = await trading.sellPrice(binance, options)
-    //   expect(sellPrice).toBe(15);
-    //   binance.setLow(undefined)
-    // });
   });
 });
